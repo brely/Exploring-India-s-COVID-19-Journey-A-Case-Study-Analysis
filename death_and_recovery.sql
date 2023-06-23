77b348376ca780c8921e0c1cce7cae4b15ca3fc9
brely <brelysrivastav@gmail.com>
2023-06-23 16:18:40 +0530

    Add files via upload

---
+++ death_and_recovery.sql
@@ -0,0 +1,42 @@
+CREATE TABLE death_and_recovery(
+   Age            INTEGER  NOT NULL
+  ,Gender         VARCHAR(1) NOT NULL
+  ,Patient_status VARCHAR(9) NOT NULL
+  ,City           VARCHAR(10) NOT NULL
+  ,State          VARCHAR(17) NOT NULL
+);
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (70,'M','Deceased','Mumbai','Maharashtra');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (85,'M','Deceased','Mumbai','Maharashtra');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (54,'F','Recovered','Bangalore','Karnataka');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (65,'M','Recovered','Chennai','Chennai');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (55,'F','Recovered','Indore','Madhya Pradesh');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (20,'M','Recovered','Udaipur','Rajasthan');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (21,'F','Recovered','Delhi','Delhi');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (45,'M','Recovered','Thane','Maharashtra');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (30,'M','Recovered','Kochi','Kerala');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (57,'M','Deceased','Howrah','West Bengal');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (49,'F','Deceased','Indore','Madhya Pradesh');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (57,'M','Deceased','Belghoria','West Bengal');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (25,'M','Deceased','Gorakhpur','Uttar Pradesh');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (55,'F','Recovered','Hyderabad','Telangana');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (26,'F','Recovered','Kashmir','Jammu and Kashmir');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (38,'M','Recovered','Amritsar','Punjab');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (44,'F','Recovered','Siliguri','West Bengal');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (57,'M','Recovered','Pune','Maharashtra');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (45,'F','Recovered','Guwahati','Assam');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (65,'F','Recovered','Dispur','Assam');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (51,'M','Deceased','Mumbai','Maharashtra');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (72,'M','Deceased','Meerut','Uttar Pradesh');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (45,'M','Recovered','Lucknow','Uttar Pradesh');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (56,'M','Deceased','Dharavi','Maharashtra');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (52,'M','Deceased','Rajkot','Gujarat');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (68,'F','Deceased','Puri','Bhubaneshwar');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (40,'F','Deceased','Bhopal','Madhya Pradesh');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (55,'F','Recovered','Shillong','Meghalaya');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (54,'M','Deceased','Jaipur','Rajasthan');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (42,'M','Recovered','Gangtok','West Bengal');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (24,'F','Deceased','dehradun','uttarakhand');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (23,'M','Recovered','Darjeeling','West Bengal');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (78,'F','Recovered','Roorkie','uttarakhand');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (54,'M','Recovered','Haridwar','uttarakhand');
+INSERT INTO death_and_recovery(Age,Gender,Patient_status,City,State) VALUES (74,'F','Recovered','Mysore','Karnataka');
